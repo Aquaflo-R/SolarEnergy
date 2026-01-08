c2e5d1f694fd34c63131d006f4ed21003b471c17
unknown <rohit.decreations748@gmail.com>
2026-01-08 14:50:09 +0530

    solorbox

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,6 +21,35 @@ function Home() {
                 <i class="ri-arrow-right-fill"></i>Get A free Quote
               </button>
             </div>
+
+            <div className="w-[550px]  h-[180px]  flex gap-2 z-10 mt-10">
+              <div
+                className="w-full h-full  flex p-2 gap-5 bg-white/15 backdrop-blur-sm 
+                    border border-white/25 rounded  "
+              >
+                <img
+                  src="/src/assets/windmill1.avif"
+                  alt=""
+                  className="object-cover rounded"
+                />
+                <div className="">
+                  <h1 className="text-2xl text-white">Alternative Energy</h1>
+                  <p className="text-white">
+                    harnesses the sun's light and heat, converting it into
+                    usable power like electricity
+                  </p>
+                </div>
+              </div>
+              <div
+                className="w-[200px]  h-full bg-white/15 backdrop-blur-sm 
+                    border border-white/25 flex items-end p-2 rounded"
+              >
+                <p className="text-white">
+                  Solar energy captures the sun's light and heat, converting it
+                  into clean,renewable electricity or thermal energy.
+                </p>
+              </div>
+            </div>
           </div>
         </section>
       </main>
