a45003992aa73eb18e37767ec98612933b2aa4a2
unknown <rohit.decreations748@gmail.com>
2026-01-08 17:49:57 +0530

    aboutUsSection

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import windmill1 from "../assets/windmill1.avif";
+import windmill2 from "../assets/windmill2.avif";
 function Home() {
   return (
     <>
@@ -50,6 +51,70 @@ function Home() {
                   into clean,renewable electricity or thermal energy.
                 </p>
               </div>
+            </div>
+          </div>
+        </section>
+        <section id="About-Us" className="w-full h-screen flex">
+          <div id="aboutUsText" className="w-[40%] p-10">
+            <h3 className="text-[#001A00] font-bold bg-[#F2F2F2] inline-block py-2 px-5 rounded-4xl mt-5">
+              About us
+            </h3>
+            <h1 className="text-4xl font-bold text-[#004700] mt-20">
+              The Green Power
+            </h1>
+            <p className="text-xl mt-5">
+              The Green Power is our commitment to a cleaner, smarter, and more
+              sustainable future. We believe energy should not harm the planet —
+              it should protect it. By using solar and renewable solutions, we
+              help homes and businesses reduce carbon emissions, lower
+              electricity costs, and move towards energy independence. Our
+              systems are designed to deliver maximum efficiency while
+              respecting nature.<br></br>
+              <br></br> We don’t just generate power — we generate impact.
+            </p>
+            <button className="bg-[#C7F43E] text-[#001A00] py-2 px-5 rounded-4xl mt-5">
+              Learn More
+            </button>
+          </div>
+          <div
+            id="aboutUsImg"
+            className=" relative flex flex-col justify-center ml-40"
+          >
+            <img
+              src={windmill2}
+              alt=""
+              className="w-full h-[80%] object-cover rounded"
+            />
+            {/* absolute Value */}
+            <div
+              className="absolute w-3xs bg-[#C7F43E]/40 backdrop-blur-md 
+                    border border-white/25 rounded p-2  left-50 bottom-30"
+            >
+              <h1 className="text-2xl text-[#001A00] font-bold ">
+                05+
+                <span className="text-xl text-[#001A00] font-medium">
+                  Experience
+                </span>
+              </h1>
+              <p className="text-[#001A00] text-sm mt-2">
+                hands-on experience delivering efficient and eco-friendly solar
+                energy solutions for a sustainable tomorrow
+              </p>
+            </div>
+            <div
+              className="absolute w-3xs bg-[#C7F43E]/40 backdrop-blur-md 
+                    border border-white/25 rounded p-2  left-85 top-30"
+            >
+              <h1 className="text-2xl text-[#001A00] font-bold ">
+                87%
+                <span className="text-xl text-[#001A00] font-medium">
+                  Success
+                </span>
+              </h1>
+              <p className="text-[#001A00] text-sm mt-2">
+                87% project success rate achieved through reliable planning,
+                skilled execution, and high-quality solar solutions.
+              </p>
             </div>
           </div>
         </section>
